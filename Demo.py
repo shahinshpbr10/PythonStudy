51e23572213c087270b660d7d00e18d105f5e2c6
SHAHINSH <123875287+shahinshpbr10@users.noreply.github.com>
2025-07-23 06:21:34 +0000

    some more feeds for babies

--- Demo.py
+++ Demo.py
@@ -1,8 +1,30 @@
-person = {
-    "name": "Shanu",
-    "age": 23,
-    "city": "Perintalmanna",
-    "Hobies": ["Reading", "Travelling", "Cooking"]
-}
+# person = {
+#     "name": "Shanu",
+#     "age": 23,
+#     "city": "Perintalmanna",
+#     "Hobies": ["Reading", "Travelling", "Cooking"]
+# }
 
-print(person["Hobies"][0])
+# print(person["Hobies"][0])
+
+# count=9
+# while count>0:
+#   print(count)
+#   count-=1
+
+
+
+# for i in range(5):
+#   if i==3:
+#     break
+#   print(i)  
+
+# for i in range (5):
+#   if i==3:
+#     continue
+#   print(i)
+
+def area_of_rectangle(length, breadth) :
+   return f"The area of rectangle is {length * breadth}"
+
+print(area_of_rectangle(5, 6))
